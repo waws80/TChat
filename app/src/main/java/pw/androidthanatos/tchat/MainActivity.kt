db2b004ed179390fc983463bf45e26d3d3f10a09
thanatos <523928638@qq.com>
2019-12-24 22:03:02 +0800

    Initial commit

---
+++ app/src/main/java/pw/androidthanatos/tchat/MainActivity.kt
@@ -0,0 +1,18 @@
+package pw.androidthanatos.tchat
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+/**
+ * 主类
+ *@author thanatos
+ *@data 2019/12/24 10:01 PM
+ *@version 1.0.0
+ */
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
